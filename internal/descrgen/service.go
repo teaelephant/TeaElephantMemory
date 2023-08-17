e29397fd40811484885a73d122e43c8e30acac14
khasanov.andrey <akhasanov@aicorner.org>
2023-08-18 01:09:22 +0300

    - add logs to description generation

--- internal/descrgen/service.go
+++ internal/descrgen/service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/sashabaranov/go-openai"
+	"github.com/sirupsen/logrus"
 )
 
 const requestTemplate = "Опиши взвешенно и информативно без маркетинга чай %s, чтобы помочь сделать выбор человеку на основании вкусовых качеств, пользы для организма, стимуляции к деятельности"
@@ -15,6 +16,7 @@ type DescriptionGenerator interface {
 
 type generator struct {
 	client *openai.Client
+	log    *logrus.Entry
 }
 
 func (g *generator) GenerateDescription(ctx context.Context, name string) (string, error) {
@@ -32,12 +34,15 @@ func (g *generator) GenerateDescription(ctx context.Context, name string) (strin
 	)
 
 	if err != nil {
+		g.log.WithError(err).Error("description generation error")
 		return "", err
 	}
+
+	g.log.WithField("response", resp).Error("description generation result")
 
 	return resp.Choices[0].Message.Content, nil
 }
 
-func NewGenerator(token string) DescriptionGenerator {
-	return &generator{client: openai.NewClient(token)}
+func NewGenerator(token string, log *logrus.Entry) DescriptionGenerator {
+	return &generator{client: openai.NewClient(token), log: log}
 }
